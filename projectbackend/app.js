ea1fd3e616352dbaedf4404f7a47eb225e844a36
calee14 <caplee1000@gmail.com>
2018-03-12 20:21:11 -0700

    working on home page

--- projectbackend/app.js
+++ projectbackend/app.js
@@ -3,13 +3,14 @@ var express = require('express');
 var exphbs  = require('express-handlebars');
 var app = express()
 
+app.use(express.static('public'));
 /* set main layout to be main.handlebars */
-app.engine('handlebars', exphbs({defautLayout: 'main'}));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 /* set our view engine to be handlebars */
 app.set('view engine', 'handlebars');
 
 app.get('/', function(req, res) {
-	res.render("temp", {gifUrl: "http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif"})
+	res.render("home", {msg: ["Hello World", "Hello Word", "Hello World"]});
 })
 /* Hello World (Temporary)*/
 app.get('/Hello World', function(req, res) {
