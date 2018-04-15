a529478bcf23fa91cf92bfc41ff29e75ba80c1de
calee14 <caplee1000@gmail.com>
2018-04-15 16:44:31 -0700

    working on connecting database to website

--- projectbackend/app.js
+++ projectbackend/app.js
@@ -1,7 +1,11 @@
 /* import modules */
 var express = require('express');
 var exphbs  = require('express-handlebars');
-var app = express()
+var pg = require("pg");
+var app = express();
+
+/* connection string to for db */
+var connectionString = "postgres://postgres:capsdatabase@localhost:5432/careersearchdb"
 
 app.use(express.static('public'));
 
@@ -11,8 +15,22 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', function(req, res) {
+	// pg.connect(connectionString, function (err, client, done) {
+	// 	if(err) {
+	// 		console.log("not able to get connection " + err);
+	// 		res.status(400).send(err);
+	// 	}
+	// 	client.query('SELECT * FROM majoroccupations', function(err, result) {
+	// 		done();
+	// 		if(err) {
+	// 			console.log(err);
+	// 			res.status(400).send(err);
+	// 		}
+	// 		res.status(200).send(result.rows);
+	// 	});
+	// });
 	res.render("home", {msg: ["Hello World", "Hello Word", "Hello World"]});
-})
+});
 
 let row = [
 	{careers: ['sdf', 'sd']},
