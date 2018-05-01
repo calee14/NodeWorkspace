6016da92cc64d0db35a44d66154ecf58d518b806
calee14 <caplee1000@gmail.com>
2018-04-30 21:38:15 -0700

    added skills for career

--- projectbackend/models/query_tools.js
+++ projectbackend/models/query_tools.js
@@ -2,6 +2,37 @@ module.exports = {
 	getDuties: function(object, index) {
 		return object[index][0]["duties"];
 	},
+	getHowToBecome: function(object, index) {
+		var new_object = object[index][0];
+		for(var key in new_object) {
+			if(new_object.hasOwnProperty(key)) {
+				console.log(key + " -> " + new_object[key][0]);
+			}
+		}
+	},
+	getSkills: function(object, index) {
+		var new_object = object[index][0];
+		var count = 0;
+		for(var key in new_object) {
+			if(count < 2) {count += 1; continue;}
+			if(new_object.hasOwnProperty(key)) {
+				howtobecome = new_object[key];
+				if(howtobecome == null) {continue;}
+				for (var i = 0; i < howtobecome.length; i++) {
+					if(howtobecome[i].includes("skills") || howtobecome[i].includes("skills")) {
+						var skills = []
+						for (var i = 0; i < howtobecome.length; i+=2) {
+							skills.push(howtobecome[i] + " "+ howtobecome[i+1]);
+						}
+						console.log(skills)
+						return skills;
+					}
+				}
+			}
+			count += 1;
+		}
+		return -1;
+	},
 	getPoints: function(object, index) {
 		var current_e = parseInt(object[index][0]["employment_2016_0"].replace(/,/g, ""));
 		var projected_e = parseInt(object[index][0]["projected_employment_2026_0"].replace(/,/g, ""));
