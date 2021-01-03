1a918843e86b81ee03be87a42ff7a89dba8f2da7
calee14 <caplee1000@gmail.com>
2021-01-03 15:30:15 -0800

    add constructors to classes

--- reactprac/my-app/src/index.js
+++ reactprac/my-app/src/index.js
@@ -3,18 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares = Array(9).fill(null)
+    }
+  }
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)}
+      />;)
+    ;
   }
 
   render() {
