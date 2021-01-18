e08526831d637b765464c5e4743b1f1d122ffddc
calee14 <caplee1000@gmail.com>
2021-01-17 16:33:13 -0800

    add name

--- nextjs/nextjs-blog/components/layout.js
+++ nextjs/nextjs-blog/components/layout.js
@@ -3,7 +3,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Your Name'
+const name = 'Cappillen Lee'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
