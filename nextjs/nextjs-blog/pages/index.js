4f5eb43c2f2f14dfa55bfcef00e3b77fa0c51006
calee14 <caplee1000@gmail.com>
2021-01-18 17:38:01 -0800

    get posts and pass to home

--- nextjs/nextjs-blog/pages/index.js
+++ nextjs/nextjs-blog/pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -17,4 +18,13 @@ export default function Home() {
       </section>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
