9ed5da0324f19dd71742361a133caab57e00bf21
calee14 <caplee1000@gmail.com>
2021-01-18 17:39:41 -0800

    display posts data

--- nextjs/nextjs-blog/pages/index.js
+++ nextjs/nextjs-blog/pages/index.js
@@ -16,6 +16,21 @@ export default function Home({ allPostsData }) {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   )
 }
