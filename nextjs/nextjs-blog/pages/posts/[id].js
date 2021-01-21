3e198210bd6cfe45b4a86a5c099f6d38e338d1d3
calee14 <caplee1000@gmail.com>
2021-01-20 18:46:20 -0800

    add dynamic routes for static pages

--- nextjs/nextjs-blog/pages/posts/[id].js
+++ nextjs/nextjs-blog/pages/posts/[id].js
@@ -1,5 +1,14 @@
 import Layout from '../../components/layout'
+import { getAllPostIds } from '../../lin/posts'
 
 export default function Post() {
   return <Layout>...</Layout>
+}
+
+export async function getStaticPaths() {
+	const paths = getAllPostIds()
+	return {
+		paths,
+		fallback: false
+	}
 }
