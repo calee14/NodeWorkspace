0662e675cb43a29707a441ab001c31fa121bc73c
calee14 <caplee1000@gmail.com>
2021-01-20 18:52:14 -0800

    add layout

--- nextjs/nextjs-blog/pages/posts/[id].js
+++ nextjs/nextjs-blog/pages/posts/[id].js
@@ -2,7 +2,15 @@ import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export default function Post() {
-  return <Layout>...</Layout>
+  return (
+  <Layout>
+  	{postData.title}
+  	<br />
+  	{postData.id}
+  	<br />
+  	{postData.date}
+  </Layout>
+  )
 }
 
 export async function getStaticPaths() {
