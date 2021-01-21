813c45b48ee6f0ea260fb1f2dcb8168d4334cc9c
calee14 <caplee1000@gmail.com>
2021-01-20 18:51:24 -0800

    add stat prop func get post data

--- nextjs/nextjs-blog/pages/posts/[id].js
+++ nextjs/nextjs-blog/pages/posts/[id].js
@@ -1,5 +1,5 @@
 import Layout from '../../components/layout'
-import { getAllPostIds } from '../../lin/posts'
+import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export default function Post() {
   return <Layout>...</Layout>
@@ -11,4 +11,14 @@ export async function getStaticPaths() {
 		paths,
 		fallback: false
 	}
+}
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id)
+  console.log(postData)
+  return {
+    props: {
+      postData
+    }
+  }
 }
