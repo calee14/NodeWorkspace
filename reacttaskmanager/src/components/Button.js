68a3b5b4f2abe172fece33737fb86641a5299b14
calee14 <caplee1000@gmail.com>
2021-05-12 01:47:42 -0700

    add props and onclick for button comp

--- reacttaskmanager/src/components/Button.js
+++ reacttaskmanager/src/components/Button.js
@@ -1,5 +1,18 @@
-const Button = ({ color, text }) => {
-    return <button style={{backgroundColor:color}} className='btn'>{text}</button>
+import PropTypes from 'prop-types'
+
+const Button = ({ color, text, onClick}) => {
+    return <button onClick={onClick} style={{backgroundColor:color}} className='btn'>
+        {text}
+    </button>
 }
 
+Button.defaultProps = {
+    color: 'steelBlue',
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+    onClick: PropTypes.func,
+}
 export default Button
