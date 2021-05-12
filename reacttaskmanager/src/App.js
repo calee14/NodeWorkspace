b25a72fe2ec045d18f47b5ffee829a5cc9e36458
calee14 <caplee1000@gmail.com>
2021-05-12 01:48:11 -0700

    add title to app conntainer

--- reacttaskmanager/src/App.js
+++ reacttaskmanager/src/App.js
@@ -1,7 +1,9 @@
+import Header from './components/Header'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="container">
+      <Header title='Task Tracker'/>
     </div>
   );
 }
