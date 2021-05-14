c9bb86412c526eb2806d1a7286a0e6bdcfee1af3
calee14 <caplee1000@gmail.com>
2021-05-14 01:11:07 -0700

    pass func down to task

--- reacttaskmanager/src/components/Tasks.js
+++ reacttaskmanager/src/components/Tasks.js
@@ -1,13 +1,13 @@
 import React from 'react'
-
+import Task from './Task'
 
 // Use key tag for when mapping through a list and putting it in an html element
-const Tasks = ({ tasks }) => { // destructure the props obeject with curly brackets
+const Tasks = ({ tasks, onDelete, onToggle }) => { // destructure the props obeject with curly brackets
     
     return (
         <>
         {tasks.map((task) => (
-            <h3 key={task.id}>{task.text}</h3>
+            <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} />
         ))}
         </>
     )
