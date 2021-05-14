69c9637113867b84707b7548d015a249cb77c449
calee14 <caplee1000@gmail.com>
2021-05-14 00:52:09 -0700

    receive tasks through props

--- reacttaskmanager/src/components/Tasks.js
+++ reacttaskmanager/src/components/Tasks.js
@@ -1,30 +1,9 @@
 import React from 'react'
-import { useState } from 'react'
 
-const tasksDefault = [
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true
-    },
-    {
-        id: 2,
-        text: 'Coach Appointment',
-        day: 'Feb 8th at 2:30pm',
-        reminder: true
-    },
-    {
-        id: 3,
-        text: 'Send Mission Bit Email',
-        day: 'Feb 15th at 2:30pm',
-        reminder: false
-    },
-]
 
 // Use key tag for when mapping through a list and putting it in an html element
-const Tasks = () => {
-    const [tasks, setTasks] = useState(tasksDefault) // var in between useState is default
+const Tasks = ({ tasks }) => { // destructure the props obeject with curly brackets
+    
     return (
         <>
         {tasks.map((task) => (
