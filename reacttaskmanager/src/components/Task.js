243eb078a15046f1e6f33c3b4e5493689c4254f4
calee14 <caplee1000@gmail.com>
2021-05-14 01:19:35 -0700

    change css based on task

--- reacttaskmanager/src/components/Task.js
+++ reacttaskmanager/src/components/Task.js
@@ -3,7 +3,7 @@ import { FaTimes } from 'react-icons/fa'
 const Task = ({ task, onDelete, onToggle }) => {
     // Can choose the variable to send back up on callback
     return (
-        <div className="task" onDoubleClick={() => onToggle(task.id)}>
+        <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}>
             <h3>{task.text}<FaTimes style={{color: 'blue', cursor: 'pointer'}} onClick={() => onDelete(task.id)} /></h3>
             <p>{task.day}</p>
         </div>
