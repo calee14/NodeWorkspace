f8d8d85429fe3161593ca25ee496adec1d7ad331
calee14 <caplee1000@gmail.com>
2021-06-14 01:19:36 -0700

    comment on concat in react

--- reacttaskmanager/src/components/Task.js
+++ reacttaskmanager/src/components/Task.js
@@ -2,6 +2,7 @@ import { FaTimes } from 'react-icons/fa'
 
 const Task = ({ task, onDelete, onToggle }) => {
     // Can choose the variable to send back up on callback
+    // Concat strings with an if statement for the reminder
     return (
         <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}>
             <h3>{task.text}<FaTimes style={{color: 'blue', cursor: 'pointer'}} onClick={() => onDelete(task.id)} /></h3>
