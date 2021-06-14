768cb3f9af8bfd462236a0670316e5d63a2ef8da
calee14 <caplee1000@gmail.com>
2021-06-14 13:28:06 -0700

    add hook for loc router

--- reacttaskmanager/src/components/Header.js
+++ reacttaskmanager/src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Button from './Button'
+import { useLocation } from 'react-router-dom'
 
 const Header = ({ title, onAdd, showAdd }) => {
     // Can pass func as a prop to component
@@ -8,11 +9,15 @@ const Header = ({ title, onAdd, showAdd }) => {
         console.log('click')
     }
 
+    const location = useLocation()
+
     // For react use {} in html for dynamic styles
+    // also in react use {} if need to put condition or func
+    // use () if code fragment needs to be more than one line
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
+            {location.pathname === '/' && <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>}
         </header>
     )
 }
