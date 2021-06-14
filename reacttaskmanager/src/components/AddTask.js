035307c5ce1455aceeb5ef1c95e21c0db8f72d87
calee14 <caplee1000@gmail.com>
2021-06-14 01:17:55 -0700

    add submit events and pass params to func

--- reacttaskmanager/src/components/AddTask.js
+++ reacttaskmanager/src/components/AddTask.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import { useState } from 'react'
 
-const AddTask = () => {
-    const [text, setText] = useState('');
+const AddTask = ({ onAdd }) => {
+    const [text, setText] = useState('') // create vars with states for the forms
     const [day, setDay] = useState('')
     const [reminder, setReminder] = useState(false)
 
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        if(!text) {
+            alert('Please add a task')
+            return
+        }
+
+        onAdd({ text, day, reminder }) // pass in the parameters for the func obj passed as a param
+
+        setText('') // clear the form var
+        setDay('')
+        setReminder(false)
+    }
     return (
         <>
-        <form className='add-form'>
+        <form className='add-form' onSubmit={onSubmit}>
             <div className='form-control'>
                 <label>Task</label>
                 <input type='text' placeholder='Add Task' value={text} onChange={(e)=>setText(e.target.value)}/>
@@ -19,7 +33,7 @@ const AddTask = () => {
             </div>
             <div className='form-control form-control-check'>
                 <label>Set Reminder</label>
-                <input type='checkbox' value={reminder} onChange={(e)=>setReminder(e.currentTarget.checked)}/>
+                <input type='checkbox' checked={reminder} value={reminder} onChange={(e)=>setReminder(e.currentTarget.checked)}/>
             </div>
             <input className='btn btn-block' type='submit' value='Save'/>
         </form>
