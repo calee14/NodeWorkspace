e2b17e9c2d135f985071500ed05e1f5d19265231
calee14 <caplee1000@gmail.com>
2021-06-14 00:59:23 -0700

    add task component

--- reacttaskmanager/src/App.js
+++ reacttaskmanager/src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import { useState } from 'react'
+import AddTask from './components/AddTask'
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -40,6 +41,7 @@ function App() {
   return (
     <div className="container">
       <Header title='Task Tracker'/>
+      <AddTask/>
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'There are no tasks to show.'}
     </div>
   );
