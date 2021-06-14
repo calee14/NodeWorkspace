6b5d899772ff9c19196444633550355e8e701531
calee14 <caplee1000@gmail.com>
2021-06-14 00:59:51 -0700

    add task component react source

---
+++ reacttaskmanager/src/components/AddTask.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useState } from 'react'
+
+const AddTask = () => {
+    const [text, setText] = useState('');
+    const [day, setDay] = useState('')
+    const [reminder, setReminder] = useState(false)
+
+    return (
+        <>
+        <form className='add-form'>
+            <div className='form-control'>
+                <label>Task</label>
+                <input type='text' placeholder='Add Task' value={text} onChange={(e)=>setText(e.target.value)}/>
+            </div>
+            <div className='form-control'>
+                <label>Day & Time</label>
+                <input type='text' placeholder='Add Day and Time' value={day} onChange={(e)=>setDay(e.target.value)}/>
+            </div>
+            <div className='form-control form-control-check'>
+                <label>Set Reminder</label>
+                <input type='checkbox' value={reminder} onChange={(e)=>setReminder(e.currentTarget.checked)}/>
+            </div>
+            <input className='btn btn-block' type='submit' value='Save'/>
+        </form>
+        </>
+    )
+}
+
+export default AddTask
