4b695fdc4451b55e7b3de7e8484ac4a44b0a07b8
calee14 <caplee1000@gmail.com>
2021-07-08 15:16:07 -0700

    add apollo for client render

--- next-with-apollo/pages/_app.js
+++ next-with-apollo/pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import { ApolloProvider } from "@apollo/client"
+import client from "../apollo-client"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // use the apollo provider to allow client side rendering
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+    )
 }
 
 export default MyApp
