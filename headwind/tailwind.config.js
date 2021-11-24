09d3f67d3456562f81c26f62cf7e3f8c80d90c19
calee14 <caplee1000@gmail.com>
2021-11-24 11:40:32 -0800

    add colors and purge unused css

--- headwind/tailwind.config.js
+++ headwind/tailwind.config.js
@@ -1,9 +1,17 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   mode: 'jit',
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], // for purging unused css
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#202225",
+        secondary: "#5865f2",
+        gray: colors.trueGray,
+      }
+    },
   },
   variants: {
     extend: {},
