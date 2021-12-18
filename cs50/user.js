72ac036b5a1f0e9d5233b6e742054948b5915000
calee14 <caplee1000@gmail.com>
2021-12-17 18:31:45 -0800

    user schema address before replacing with address

--- cs50/user.js
+++ cs50/user.js
@@ -1,18 +1,31 @@
 const mongoose = require('mongoose');
 
+// create an address schema to replace the address prop in userSchema
+const addressSchema = new mongoose.Schema({
+    street: String,
+    city: String, 
+});
+
 const userSchema = new mongoose.Schema({ // define the fields that the schema will take in
     name: String,
     age: Number,
     email: String,
     bestFriend: { 
         type: mongoose.SchemaTypes.ObjectId,
-        ref: userSchema, 
         index: true
     },
+    hobbies: [String], // an array of strings
+    address: { // can have nested properties
+        street: String,
+        city: String,
+    },
+    createdAt: Date,
+    updatedAt: Date,
+}, {
     timestamps: {
-        createdAt: true,
-        updatedAt: true,
+        createdAt: 'createdAt',
+        updatedAt: 'updatedAt',
     }
-})
+});
 
 module.exports = mongoose.model('User', userSchema);
