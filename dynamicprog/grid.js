ddbb653299919ad9fb1bd25dc47199cfff846091
calee14 <caplee1000@gmail.com>
2022-01-04 22:45:42 -0800

    memo the grid traveler

--- dynamicprog/grid.js
+++ dynamicprog/grid.js
@@ -7,16 +7,22 @@
         base case
 */
 
-const gridTraveler = (m, n) => {
+const gridTraveler = (m, n, memo={}) => {
+    // are the args in the memo
+    const key = m + ',' + n;
+    if (key in memo) return memo[key];
     if (m === 1 && n === 1) return 1;
     if (m === 0 || n === 0) return 0;
 
-    return gridTraveler(m-1, n) + gridTraveler(m, n-1);
+    memo[key] = gridTraveler(m-1, n, memo) + gridTraveler(m, n-1, memo);
+    return memo[key];
 }
 
 console.log(gridTraveler(1, 1));
-console.log(gridTraveler(2, 3));
+console.log(gridTraveler(2, 3)); // O(2^n+m) time
+                                // O(n+m) space
 console.log(gridTraveler(3, 2));
 console.log(gridTraveler(3, 3));
-console.log(gridTraveler(18, 18));
+console.log(gridTraveler(18, 18)); // O(n * m) time
+                                // O(n + m) space
 
