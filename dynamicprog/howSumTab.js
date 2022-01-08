83dffb0ec7a0a09ad7c759fb6727639d997fe504
calee14 <caplee1000@gmail.com>
2022-01-08 14:41:58 -0800

    tabulated howSum

---
+++ dynamicprog/howSumTab.js
@@ -0,0 +1,18 @@
+const howSum = (targetSum, numbers) => {
+    const table = Array(targetSum+1).fill(null);
+    table[0] = [];
+
+    for(let i=0;i<=table.length;i++) {
+        for(let num of numbers) {
+            if(table[i] !== null && i+num < table.length) {
+                table[i+num] = [...table[i], num];
+            }
+        }
+    }
+    return table[targetSum]
+}
+
+
+console.log(howSum(7, [2, 3]));
+console.log(howSum(7, [4, 3, 5, 2]));
+console.log(howSum(300, [14, 7]));
