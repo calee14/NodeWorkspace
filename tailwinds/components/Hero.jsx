ba47db26e2f0bfe9f3108b1248381cc69de13435
calee14 <caplee1000@gmail.com>
2022-01-22 21:51:19 -0800

    learn flex

--- tailwinds/components/Hero.jsx
+++ tailwinds/components/Hero.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const Hero = () => {
     return (
         <>
@@ -5,6 +7,27 @@ const Hero = () => {
                 <h1 className="lg:text-9xl md:text-7xl sm:text-5xl text-3xl font-black mb-14">
                     UCSB Debates
                 </h1>
+                <Link href="/">
+                    <div className="py-6 px-10 bg-yellow-500 rounded-full 
+                        text-3xl hover:bg-yellow-300 transition 
+                        duration-300 ease-in-out flex items-center 
+                        animate-bounce">
+                        <a className="flex-1">Order now</a>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-6 w-6" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 
+                            2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4
+                             2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                        </svg>
+                    </div>
+                </Link>
             </div>
         </>
     )
